941671f50ee680fa007a847d3f1894c491a21ccc
pgb123au <39262348+pgb123au@users.noreply.github.com>
2026-07-05 12:28:06 +1000

    chore: new custom hero/feature images + corrected logo wordmark + dash purge (2026-07-04 image pass)

--- netlify/functions/forward-result.js
+++ netlify/functions/forward-result.js
@@ -7,7 +7,7 @@
 
 const TO_EMAIL = "peter@yesai.au";
 const FROM_EMAIL = "voicemail@yesai.au";
-const FROM_NAME = "Heritage Springs Lakes Paving Co &amp; Pergolas";
+const FROM_NAME = "Officer Paving Co";
 
 exports.handler = async (event) => {
   if (event.httpMethod !== "POST") return { statusCode: 405, body: "Method not allowed" };
